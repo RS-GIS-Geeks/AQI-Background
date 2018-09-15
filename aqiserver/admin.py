3be0917be3cc2983a96244c54a0827bae8fef356
CS-Tao <990296951@qq.com>
2018-09-15 19:36:16 +0800

    注册模型

--- aqiserver/admin.py
+++ aqiserver/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from aqiserver import models
 
 # Register your models here.
+admin.site.register(models.Aqidaydata)
+admin.site.register(models.Aqimonthdata)
+admin.site.register(models.City)
+admin.site.register(models.Province)
